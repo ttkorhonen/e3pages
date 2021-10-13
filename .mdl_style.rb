140940fa0ca8318e37a39c11982df8356a69849c
Wayne Lewis <wayne.lewis@ess.eu>
2021-10-13 16:38:03 -0700

    Add comments to style file

--- .mdl_style.rb
+++ .mdl_style.rb
@@ -9,7 +9,11 @@ rule 'MD013', :code_blocks => false
 rule 'MD029', :style => :ordered
 # Allow leading $ sign in console commands
 exclude_rule 'MD014'
+# Allow html tag syntax (<tag>) in docs. Not used as tags in
+# our case, but to show command line options.
 exclude_rule 'MD033'
+# Allow bare URLs
 exclude_rule 'MD034'
-# Allow for anchors at start of file, otherwise they are not found in cross-referencing
+# Allow for anchors at start of file, otherwise they are
+# not found in cross-referencing
 exclude_rule 'MD041'
