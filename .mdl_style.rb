da001ae3bc59f92cc4c9b889d6a3e61bbe99df6b
Wayne Lewis <wayne.lewis@ess.eu>
2021-10-14 08:49:51 -0700

    Add additional comments to style file

--- .mdl_style.rb
+++ .mdl_style.rb
@@ -1,3 +1,6 @@
+# Rules are defined here: https://github.com/markdownlint/markdownlint/blob/master/docs/RULES.md
+#
+# This file defines custom style selections used for ESS documentation
 all
 # Set unordered list preference
 rule 'MD004', :style => :consistent
@@ -11,10 +14,11 @@ rule 'MD024', :allow_different_nesting => true
 rule 'MD029', :style => :ordered
 # Allow leading $ sign in console commands
 exclude_rule 'MD014'
-# Allow html tag syntax (<tag>) in docs. Not used as tags in
-# our case, but to show command line options.
+# Allow html tag syntax (i.e. <tag>) in docs. Not used as tags in our case, but
+# to show command line options.
 exclude_rule 'MD033'
-# Allow bare URLs
+# Allow bare URLs as the [text](URL) format in Sphinx/MyST markdown allows that
+# format, but it fails the MD034 test
 exclude_rule 'MD034'
 # Allow for anchors at start of file, otherwise they are
 # not found in cross-referencing
